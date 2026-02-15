72acd10a90e7b591c6e11497ee71c320af56038d
yvetalkyogre <yveltalkyogrearceus@gmail.com>
2026-02-15 22:46:29 +0000

    Add files via upload

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -8990,4 +8990,9 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		doublesTier: "(DUU)",
 		natDexTier: "Uber",
 	},
+	shinyarceus: {
+		tier: "Uber",
+		doublesTier: "DUber",
+		natDexTier: "Uber",
+	},
 };
